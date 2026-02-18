ee7133bfea9b75c88b33985284da29df304652ca
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-02-18 12:22:05 -0500

    Fetch function for single item

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import { getCollection } from './fetch-helpers.js'
 import { renderCollection } from './dom-helpers.js';
-
+import { renderSingleItem } from './dom-helpers.js';
+ 
 getCollection().then((data) => {
   if (data === null) {
     console.log("failed to load")
