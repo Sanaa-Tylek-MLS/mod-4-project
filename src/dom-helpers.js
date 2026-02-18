b940a3fa44d8e1d8cd8b4d5053cb1cd63a5cf2c2
tylekmj-ops <tylekmj@gmail.com>
2026-02-18 10:33:38 -0500

    collection feature completed

---
+++ src/dom-helpers.js
@@ -0,0 +1,30 @@
+export const renderCollection = (data) => {
+  console.log(data);
+  
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  for (let i = 0; i < data.length; i++) {
+    const imgID = data[i]["image_id"];
+    const title = data[i]["title"];
+
+    const section = document.createElement("section");
+    section.classList.add("container");
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const p = document.createElement("p");
+    p.classList.add("title");
+    p.textContent = title;
+
+    const img = document.createElement("img");
+    img.src = `https://www.artic.edu/iiif/2/${imgID}/full/843,/0/default.jpg`;
+    img.classList.add("image");
+
+    card.appendChild(p);
+    card.appendChild(img);
+    section.appendChild(card);
+    app.appendChild(section);
+  }
+};
---
+++ src/fetch-helpers.js
@@ -0,0 +1,16 @@
+export const getCollection = async () => {
+  try {
+    const response = await fetch(
+      "https://api.artic.edu/api/v1/artworks/search?q=landscape&query[term][is_public_domain]=true&limit=100&fields=id,title,image_id,artwork_type_title",
+    );
+
+    if (!response.ok) {
+      throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
+};
